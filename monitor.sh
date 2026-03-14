72ff32f045ac92e843a8eb34bf0ed695bc7f24a8
Artak Sargsyan <sartak812@gmail.com>
2026-03-14 20:38:08 +0400

    chore(repo): flatten CryptoPulse to HOMEWORK6-only project

--- monitor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# ---------------------------------------------------
-# BDG Corp Server Monitoring Script
-# ---------------------------------------------------
-echo "System Check: RAM is $(free -m | awk 'NR==2 {print $7 " MB"}') | Disk free is $(df / | awk 'NR==2 {print 100-$5 "%"}')" 
